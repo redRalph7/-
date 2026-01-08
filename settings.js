528ca0e0cef77c3c7fc2313dc7257c9c7eb1e0bb
redRalph7 <redralph777@gmail.com>
2026-01-08 20:19:55 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: '꧁༒☬𝓡𝓮𝓭 𝓡𝓪𝓵𝓹𝓱 𝔁 𝓓𝓲𝓰𝓰𝓪 𝓓☬༒꧂',
+  author: 'Red Ralph‎',
+  botName: "꧁༒☬𝓡𝓮𝓭'𝓡𝓪𝓵𝓹𝓱 𝔁 𝓓𝓲𝓰𝓰𝓪 𝓓☬༒꧂",
+  botOwner: 'Red Ralph', // Your name
+  ownerNumber: '2348062539987', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "On The G-ting still 🇬🇧🗡️🔫.",
   version: "3.0.5",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "",
 };
 
 module.exports = settings;
